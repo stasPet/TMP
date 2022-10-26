fe26ed32b2a8cd91e7d2aab79058ba0f189f4f94
Stanislav <petrov_stas@outlook.com>
2022-10-26 19:06:19 +0300

    test

--- TMP/main.cpp
+++ TMP/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <array>
 
+template< typename Т, std::size_t N >
+struct DotProductT
+{
+  static inline Т result( Т const * a, Т const * b )
+  {
+    return *a * *b + DotProductT< Т, N - 1 >::result( a + 1, b + 1 );
+  }
+};
+
+// Частичная специализация в качестве критерия остановки
+template< typename Т >
+struct DotProductT< Т, 0 >
+{
+  static inline Т result( Т const *, Т const * )
+  {
+    return Т{};
+  }
+};
+
+template< typename Т, std::size_t N>
+auto dotProduct(std::array< Т, N > const & x, 
+                std::array< Т, N > const & y)
+{
+  return DotProductT< Т, N >::result( x.data(), y.data() );
+}
 
 
 int main()
 {
-
+  auto a { &"1" };
+  auto b { &"1" };
 
   return 0;
 }
